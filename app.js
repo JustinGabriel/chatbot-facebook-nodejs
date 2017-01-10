596e6d3c5efd4ca06c0d73a8615ec1920b5d4096
Justin Gabriel <justingabriel@Justins-MacBook-Pro-3.local>
2017-01-09 23:09:26 -0700

    Extract information and send it to email

--- app.js
+++ app.js
@@ -26,6 +26,15 @@ if (!config.FB_APP_SECRET) {
 if (!config.SERVER_URL) { //used for ink to static files
 	throw new Error('missing SERVER_URL');
 }
+if (!config.SENDGRID_API_KEY) { //used for sendgrid
+	throw new Error('missing SENDGRID_API_KEY');
+}
+if (!config.EMAIL_FROM) { //email from
+	throw new Error('missing EMAIL_FROM');
+}
+if (!config.EMAIL_TO) { //email to
+	throw new Error('missing EMAIL_TO');
+}
 
 
 
@@ -184,6 +193,33 @@ function handleEcho(messageId, appId, metadata) {
 
 function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 	switch (action) {
+		case "detailed-application":
+		if (isDefined(contexts[0]) && contexts[0].name =='job_application' && contexts[0].parameters) {
+			let phone_number = (isDefined(contexts[0].parameters['phone-number'])
+				&& contexts[0].parameters['phone-number'] != '') ? contexts[0].parameters['phone-number'] : '';
+			let user_name = (isDefined(contexts[0].parameters['user-name'])
+				&& contexts[0].parameters['user-name'] != '') ? contexts[0].parameters['user-name'] : '';
+			let previous_job = (isDefined(contexts[0].parameters['previous-job'])
+				&& contexts[0].parameters['previous-job'] != '') ? contexts[0].parameters['previous-job'] : '';
+			let years_of_experience = (isDefined(contexts[0].parameters['years-of-experience'])
+				&& contexts[0].parameters['years-of-experience'] != '') ? contexts[0].parameters['years-of-experience'] : '';
+			let job_vacancy = (isDefined(contexts[0].parameters['job-vacancy'])
+				&& contexts[0].parameters['job-vacancy'] != '') ? contexts[0].parameters['job-vacancy'] : '';
+
+			if (phone_number != '' && user_name != '' && previous_job != '' && years_of_experience != '' && job_vacancy != '')
+
+				{
+					let emailContent = 'A new short form application from ' + user_name ' for the job: ' + job_vacancy +
+					'.<br> Previous job position: ' + previous_job + '.' +
+					'.<br> Years of experience: ' + years_of_experience + '.' +
+					'.<br> Phone Number: ' + phone_number + '.' ;
+
+					sendEmail('New job application', emailContent)
+
+				}
+		}
+
+		break;
 			case "job-enquiry":
 		let replies = [
 		{
@@ -866,6 +902,30 @@ function verifyRequestSignature(req, res, buf) {
 			throw new Error("Couldn't validate the request signature.");
 		}
 	}
+}
+
+function sendEmail(subject, content) {
+
+var helper = require('sendgrid').mail;
+  
+var from_email = new helper.Email(config.EMAIL_FROM);
+var to_email = new helper.Email(config.EMAIL_TO);
+var subject = subject;
+var content = new helper.Content("text/html", content);
+var mail = new helper.Mail(from_email, subject, to_email, content);
+
+var sg = require('sendgrid')(config.SENDGRID_API_KEY);
+var request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON()
+});
+
+sg.API(request, function(error, response) {
+  console.log(response.statusCode)
+  console.log(response.body)
+  console.log(response.headers)
+})
 }
 
 function isDefined(obj) {
--- config.js
+++ config.js
@@ -4,4 +4,7 @@ module.exports = {
     API_AI_CLIENT_ACCESS_TOKEN: 'edcf4375b52645728e0776359355852e',
     FB_APP_SECRET: 'bd316e285357d6543b9526b91f96f7ac',
     SERVER_URL: "https://nodejs-chatbot.herokuapp.com/",
+    SENDGRIND_API_KEY:"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",
+    EMAIL_FROM: "justinmgabriel@gmail.com",
+    EMAIL_TO: "justinmgabriel@gmail.com"
 };
